5380312cca3ab4c1fd19775c71f0f5aa8651d120
haykh <haykh.mfs+github@gmail.com>
2021-09-21 00:11:19 -0400

    view tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,66 @@
 #include <iostream>
 
 auto main() -> int {
+  using real_t = double;
   Kokkos::initialize();
   {
-    int N = 10000000;
-    double value = 16.695311, dvalue = 0.0001;
-    auto Sum = [=](const std::size_t i, double &sum) {
-      sum += 1.0 / (static_cast<double>(i) + 1.0);
-    };
+  std::size_t N{10000000};
+  Kokkos::View<real_t*> A("a", N);
+  Kokkos::View<real_t*> B("b", N);
 
-    timer::Timer timer1("kokkos");
-    timer::Timer timer2("serial");
+  // init array
+  timer::Timer timer("kokkos");
+  timer.start();
 
-    double sum1 {0.0};
-    timer1.start();
-    Kokkos::parallel_reduce(N, Sum, sum1);
-    timer1.stop();
-
-    double sum2 {0.0};
-    timer2.start();
-    for (std::size_t i{0}; i < N; ++i) {
-      Sum(i, sum2);
+  Kokkos::parallel_for("init", N,
+    KOKKOS_LAMBDA (const std::size_t n) {
+      A(n) = 1.1;
+      B(n) = 2.2;
     }
-    timer2.stop();
+  );
+
+  Kokkos::parallel_for("add", N,
+    KOKKOS_LAMBDA (const std::size_t n) {
+      A(n) = A(n) + B(n);
+    }
+  );
+
+  real_t sum{0.0};
+  Kokkos::parallel_reduce("sum", N,
+    KOKKOS_LAMBDA (const std::size_t n, real_t &sum) {
+      sum += A(n);
+    }, sum
+  );
+  timer.stop();
+  timer.printElapsed(timer::millisecond);
+  std::cout << sum / static_cast<double>(N) << "\n";
+
   }
   Kokkos::finalize();
   return 0;
 }
+
+// // **** example 1
+// int N = 10000000;
+// double value = 16.695311, dvalue = 0.0001;
+// auto Sum = [=](const std::size_t i, double &sum) {
+//   sum += 1.0 / (static_cast<double>(i) + 1.0);
+// };
+//
+// timer::Timer timer1("kokkos");
+// timer::Timer timer2("serial");
+//
+// double sum1 {0.0};
+// timer1.start();
+// Kokkos::parallel_reduce(N, Sum, sum1);
+// timer1.stop();
+//
+// double sum2 {0.0};
+// timer2.start();
+// for (std::size_t i{0}; i < N; ++i) {
+//   Sum(i, sum2);
+// }
+// timer2.stop();
+//
+// timer1.printElapsed(timer::millisecond);
+// timer2.printElapsed(timer::millisecond);
