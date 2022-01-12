e70d1be1d39ade3d03fcad428280ae1ccb77e480
haykh <haykh.mfs+github@gmail.com>
2022-01-12 16:14:00 -0500

    demo

--- demo/main-kokkos.cpp
+++ demo/main-kokkos.cpp
@@ -1,5 +1,3 @@
-#include "global.h"
-
 #include <Kokkos_Core.hpp>
 
 #include <cmath>
@@ -9,33 +7,33 @@ auto main() -> int {
   Kokkos::initialize();
   {
     int N {100000};
-    ntt::NTTArray<real_t*> evens{"evens", N};
-    ntt::NTTArray<real_t*> odds{"odds", N};
+    Kokkos::View<float*, Kokkos::HostSpace> evens {"evens", N};
+    Kokkos::View<float*, Kokkos::HostSpace> odds {"odds", N};
     
     // filling arrays
     Kokkos::parallel_for(
       "filling",
-      ntt::NTTRange<ntt::Dimension::ONE_D>({0}, {N}),
-      Lambda (const int i) {
-        evens(i) = 1.0 / SQR(2.0 * (i + 1));
-        odds(i) = 1.0 / SQR(2.0 * (i + 1) - 1.0);
+      Kokkos::RangePolicy<Kokkos::OpenMP>({0}, {N}),
+      KOKKOS_LAMBDA (const int i) {
+        evens(i) = 1.0 / (4.0 * (i + 1) * (i + 1));
+        odds(i) = 1.0 / ((2.0 * (i + 1) - 1.0) * (2.0 * (i + 1) - 1.0));
       }
     );
 
-    ntt::NTTArray<real_t*> sums{"odds", N};
+    Kokkos::View<float*, Kokkos::HostSpace> sums {"sums", N};
     Kokkos::parallel_for(
       "summing",
-      ntt::NTTRange<ntt::Dimension::ONE_D>({0}, {N}),
-      Lambda (const int i) {
+      Kokkos::RangePolicy<Kokkos::OpenMP>({0}, {N}),
+      KOKKOS_LAMBDA (const int i) {
         sums(i) = evens(i) + odds(i);
       }
     );
 
-    real_t sum {0.0};
+    float sum {0.0};
     Kokkos::parallel_reduce(
       "sum",
-      ntt::NTTRange<ntt::Dimension::ONE_D>({0}, {N}),
-      Lambda (const int i, double& s) {
+      Kokkos::RangePolicy<Kokkos::OpenMP>({0}, {N}),
+      KOKKOS_LAMBDA (const int i, float& s) {
         s += sums(i);
       }, sum
     );
